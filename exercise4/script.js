1f245dcb80c4a726bae1034b35e791f46d77c16c
Agnieszka Kurek <agniechakurek@gmail.com>
2017-07-10 09:06:50 +0200

    add exercise - very easy while loop

---
+++ exercise4/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const name = "Chris";
+let i = 0;
+while (i < 2) {
+    document.write("Happy Birthday to you!!! Happy Birthday to you !!!");
+    document.write("Happy Birthday to sweet piglet ");
+    i = i + 1;
+}
+document.write("Happy Birthday dear " + name);
