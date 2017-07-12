d6663cea44776eb7a5e92524184d964d9849cee5
Agnieszka Kurek <agniechakurek@gmail.com>
2017-07-12 08:40:50 +0200

    Make corrections in the code of the game of ships - adding missing elements

--- gameShips/script.js
+++ gameShips/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let randomLoc = Math.floor(Math.random()*5);
+let randomLoc = Math.floor(Math.random() * 5);
 let location1 = randomLoc;
 let location2 = location1 + 1;
 let location3 = location2 + 1;
@@ -10,23 +10,25 @@ let hits = 0;
 let guesses = 0;
 let isSunk = false;
 
-while (isSunk ==  false) {
+while (isSunk == false) {
     guess = prompt("Ready!!!Set the target!!!Shoot!!!(Enter a number between 0 and 6)");
-    if (guess < 0 || guess > 6){
+    if (guess < 0 || guess > 6) {
         alert("Enter a number between 0 and 6 only!!!")
-    } else {
-        guesses = guesses + 1;
-        if (guess == location1 || guess == location2 || guess == location3)
-        alert("Guess!!!");
-        hits = hits + 1;
-        if(hits == 3){
-            isSunk;
-            alert("Hit and sink!!!")
-        }
-     else { 
-        alert("You missed!!!")
     }
-    } 
+    else {
+        guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("Guess!!!");
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk;
+                alert("Hit and sink!!!")
+            }
+        }
+        else {
+            alert("You missed!!!")
+        }
+    }
 }
-const stats = " You needed " + guesses + " guesses, to sink the ship. "+ " That is your efficiency is "+ (guesses) + ".";
+const stats = " You needed " + guesses + " guesses, to sink the ship. " + " That is your efficiency is " + (3/guesses) + ".";
 alert(stats);
