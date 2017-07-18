f32be7dfb080448e665c4f61699c5e1c917e849f
Agnieszka Kurek <agniechakurek@gmail.com>
2017-07-18 07:29:24 +0200

    add  new exercise -function barkingDog

---
+++ exercise7/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function barkingDog(name, weight) {
+    if (weight > 20) {
+        console.log("Big dog barking:Woof Woof!!!");
+    } else {
+        console.log("Small dog barking:woof woof!!!");
+    }
+}
+barkingDog("Burek", 23);
+barkingDog("Mops", 13);
+barkingDog("Reksio", 30);
+barkingDog("Saba", 10);
+barkingDog("Henio", 2);
