6e15f1ceff280ea669fd34dde2ef0ed59eb07cc8
Agnieszka Kurek <agniechakurek@gmail.com>
2017-07-19 08:04:48 +0200

    add function bake

---
+++ exercise9/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function bake(degrees) {
+    let message;
+    if (degrees > 250) {
+        message = "I'm not a nuclear reactor!!!";
+    }
+    else if (degrees < 20) {
+        message = "I'm not a fridge!!!";
+    }
+    else {
+        message = "That's the right temperature for me :)";
+        // setMode("baking");
+        // setTemp(degrees);
+    }
+    return massage;
+}
+
+let status = bake(200);
+
