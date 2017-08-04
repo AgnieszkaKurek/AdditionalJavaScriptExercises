c9ff94454a458cccaf022057af2cd5362f613f93
Agnieszka Kurek <agniechakurek@gmail.com>
2017-08-04 17:16:18 +0200

    add  some variable

--- exercise12/script.js
+++ exercise12/script.js
@@ -1,2 +1,30 @@
 "use strict";
 
+var dogName = "Burek";
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log(dogName + " jaw WOOF WOOF !!!");
+} else {
+    console.log(dogName + " jaw woof woof !!!");
+}
+var dogName = "Mops";
+var dogWeight = 13;
+if (dogWeight > 20) {
+    console.log(dogName + " jaw WOOF WOOF !!!");
+} else {
+    console.log(dogName + " jaw woof woof !!!");
+}
+var dogName = "Reksio";
+var dogWeight = 53;
+if (dogWeight > 20) {
+    console.log(dogName + " jaw WOOF WOOF !!!");
+} else {
+    console.log(dogName + " jaw woof woof !!!");
+}
+var dogName = "Saba";
+var dogWeight = 17;
+if (dogWeight > 20) {
+    console.log(dogName + " jaw WOOF WOOF !!!");
+} else {
+    console.log(dogName + " jaw woof woof !!!");
+}
