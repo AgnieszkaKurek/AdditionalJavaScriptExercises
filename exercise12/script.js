7a7efab1958044f65de2ac44bf1fc383b803db66
Agnieszka Kurek <agniechakurek@gmail.com>
2017-08-04 17:34:18 +0200

    add function bark

--- exercise12/script.js
+++ exercise12/script.js
@@ -1,30 +1,14 @@
 "use strict";
 
-var dogName = "Burek";
-var dogWeight = 23;
-if (dogWeight > 20) {
-    console.log(dogName + " jaw WOOF WOOF !!!");
-} else {
-    console.log(dogName + " jaw woof woof !!!");
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " jaw WOOF WOOF !!!");
+    } else {
+        console.log(name + " jaw woof woof !!!");
+    }
 }
-var dogName = "Mops";
-var dogWeight = 13;
-if (dogWeight > 20) {
-    console.log(dogName + " jaw WOOF WOOF !!!");
-} else {
-    console.log(dogName + " jaw woof woof !!!");
-}
-var dogName = "Reksio";
-var dogWeight = 53;
-if (dogWeight > 20) {
-    console.log(dogName + " jaw WOOF WOOF !!!");
-} else {
-    console.log(dogName + " jaw woof woof !!!");
-}
-var dogName = "Saba";
-var dogWeight = 17;
-if (dogWeight > 20) {
-    console.log(dogName + " jaw WOOF WOOF !!!");
-} else {
-    console.log(dogName + " jaw woof woof !!!");
-}
+
+bark("Burek", 23);
+bark("Mops", 13);
+bark("Reksio", 53);
+bark("Burek", 17);
