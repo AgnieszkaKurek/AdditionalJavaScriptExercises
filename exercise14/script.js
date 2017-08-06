42269393f6caad1d54faf27b3a56edd7c9baa3af
Agnieszka Kurek <agniechakurek@gmail.com>
2017-08-06 15:43:13 +0200

    add funtion printAndGetHighScore

--- exercise14/script.js
+++ exercise14/script.js
@@ -3,19 +3,25 @@
 var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 var highScore = 0;
 var output = 0;
-for (var i = 0; i < scores.length; i++) {
-    console.log("Bubble liquid number " + i + " score: " + scores[i]);
-    if (scores[i] > highScore) {
-        highScore = scores[i];
+function printAndGetHighScore(scores) {
+    for (var i = 0; i < scores.length; i++) {
+        output = "Bubble liquid number " + i + " score: " + scores[i];
+        console.log(output);
+
+        if (scores[i] > highScore) {
+            highScore = scores[i];
+        }
     }
+    return highScore;
 }
+var highScore = printAndGetHighScore(scores);
 console.log("Number of tests: " + scores.length);
 console.log("The largest number of bubbles produced: " + highScore);
 
-var  bestSolutions = [];
- for (var i = 0; i < scores.length; i++){
-     if(scores[i] == highScore){
-         bestSolutions.push(i);
-     }
- }
-    console.log("Liquids with the best result: " + bestSolutions);
+var bestSolutions = [];
+for (var i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+        bestSolutions.push(i);
+    }
+}
+console.log("Liquids with the best result: " + bestSolutions);
