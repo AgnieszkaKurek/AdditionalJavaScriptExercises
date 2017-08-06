8329844408e74e7ccbf1e0e2bcda0cdd4ad28287
Agnieszka Kurek <agniechakurek@gmail.com>
2017-08-06 13:14:08 +0200

    add function building phrases

--- exercise13/script.js
+++ exercise13/script.js
@@ -1,2 +1,15 @@
 "use strict";
 
+function makePhrases() {
+    var words1 = ["continued", "multilayer", "a thousand meters", "business", "unchecked"];
+    var words2 = ["supported", "valuable", "oriented", "focused", "aligned"];
+    var words3 = ["processes", "solutions", "points", "strategy", "visions"];
+
+    var rand1 = Math.floor(Math.random() * words1.length);
+    var rand2 = Math.floor(Math.random() * words2.length);
+    var rand3 = Math.floor(Math.random() * words3.length);
+
+    var phrase = words1[rand1] + " " + words2[rand2] + " " + words3[rand3];
+    alert(phrase);
+}
+makePhrases();
