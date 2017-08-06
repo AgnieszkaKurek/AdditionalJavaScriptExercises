f8890ef156d2ffc2a0bac24fe8ec46f083f51f8d
Agnieszka Kurek <agniechakurek@gmail.com>
2017-08-06 17:59:52 +0200

    add new object cadi

--- exercise15/script.js
+++ exercise15/script.js
@@ -1,37 +1,47 @@
 "use strict";
 
 var chevy = {
-make: "Chevy",
-model: "Bel Air",
-year: 1957,
-color: "red",
-passengers: 2,
-convertible: false,
-milage: 1021
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    milage: 1021
 };
 
 var fiat = {
-make: "Fiat",
-model: "500",
-year: 1957,
-color: "pale blue",
-passengers: 2,
-convertible: false,
-milage: 88000
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "pale blue",
+    passengers: 2,
+    convertible: false,
+    milage: 88000
 };
 
 var taxi = {
-make: "SieMoCorp",
-model: "Taxi",
-year: 1955,
-color: "yellow",
-passengers: 4,
-convertible: false,
-milage: 281341
+    make: "SieMoCorp",
+    model: "Taxi",
+    year: 1955,
+    color: "yellow",
+    passengers: 4,
+    convertible: false,
+    milage: 281341
+};
+
+var cadi = {
+    make: "GM",
+    model: "Cadillac",
+    year: 1955,
+    color: "light brown",
+    passengers: 5,
+    convertible: false,
+    milage: 12892
 };
 
 function prequal(car) {
-    if (car.milage > 10000){
+    if (car.milage > 10000) {
         return false;
     } else if (car.year > 1960) {
         return false;
@@ -39,11 +49,11 @@ function prequal(car) {
     return true;
 }
 
-var worthALook = prequal(taxi);
-if (worthALook){
-    console.log("You should be interested in this " + taxi.make + " " + taxi.model);
+var worthALook = prequal(cadi);
+if (worthALook) {
+    console.log("You should be interested in this " + cadi.make + " " + cadi.model);
 } else {
-    console.log("This " + taxi.make + taxi.model + " you can forgive this purchase");
+    console.log("This " + cadi.make + " " + cadi.model + " you can forgive this purchase");
 }
 
 //console.log(fiat);
