0f3b763510e7da747f1cf3c5cb4cc9a2852c5976
Agnieszka Kurek <agniechakurek@gmail.com>
2017-08-06 17:13:45 +0200

    add new object fiat  and Reference to properties miles

--- exercise15/script.js
+++ exercise15/script.js
@@ -9,3 +9,18 @@ passengers: 2,
 convertible: false,
 milage: 1021
 };
+
+var fiat = {
+make: "Fiat",
+model: "500",
+year: 1957,
+color: "pale blue",
+passengers: 2,
+convertible: false,
+milage: 88000
+};
+
+var miles = fiat.milage;
+if (miles < 2000){
+    buyIt();
+}
