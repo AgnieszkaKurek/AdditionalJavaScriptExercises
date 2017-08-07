7cc042a852cd5deb328fa32cd645cea63e0aeb29
Agnieszka Kurek <agniechakurek@gmail.com>
2017-08-07 17:54:49 +0200

    Add function to add content to playlist

--- exercise18/script.js
+++ exercise18/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+function addSong() {
+    var song1 = document.getElementById("song1");
+    var song2 = document.getElementById("song2");
+    var song3 = document.getElementById("song3");
+
+    song1.innerHTML = "Sad suede strings - Elvis Pagelya";
+    song2.innerHTML = "Big objects in the fire - Jerry JSON Lewis";
+    song3.innerHTML = "The first line of code - Johny JavaScript";
+}
+
+window.onload = addSong;
